2eafaecd19d6daa680628ca5102c09fdc6e72be8
Damien <damien.deboudt@sfr.fr>
2022-11-18 12:51:58 +0100

    ok

--- exo 2/exercice2.js
+++ exo 2/exercice2.js
@@ -1,7 +1,7 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
 
-for( var i =56 ;  < ; )
+for( var i =56 ; i < 59; i+=2 )
 {
     document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i+"<br><br>";
 }
